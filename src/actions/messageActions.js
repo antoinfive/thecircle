4693717105eeca23b7ebf369e563fc6a8dd608f0
antoinfive <antoinfive@gmail.com>
2017-02-10 10:24:54 -0500

    add randomness to giphy search

--- src/actions/messageActions.js
+++ src/actions/messageActions.js
@@ -7,13 +7,21 @@ export function newMessageGiphySuccess(payload){
 
 export function newMessageWithGiphy(data){ 
   let adapter = new GiphyApiAdapter  
-   
+  let index = randomInt(0, 24) 
   return dispatch => { 
     return adapter.fetchRandomByMood(data.mood)
       .then((response) =>{ 
-        let giphyUrl = response.data.data[0].images.fixed_height.url
+        let giphyUrl = response.data.data[index].images.fixed_height.url
         let payload = { user: data.user, message: data.message, imageSrc: giphyUrl} 
         dispatch(newMessageGiphySuccess(payload))
       }) 
   } 
-} 
+}
+
+function randomInt(min, max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min)) + min;
+
+}
