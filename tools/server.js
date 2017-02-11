a31444ffd3a52fad8c8ac4c89c022d381fc9cde9
antoinfive <antoinfive@gmail.com>
2017-02-11 15:07:13 -0500

    add server package, socket server and client package

--- tools/server.js
+++ tools/server.js
@@ -4,12 +4,16 @@ import path from 'path';
 import config from '../webpack.config.dev';  
 import open from 'open';  
 import favicon from 'serve-favicon';
+import { Server } from 'http'
+import socket from 'socket.io'
 
 /* eslint-disable no-console */
 
-const port = 3000;  
+const port = 8000;  
 const app = express();  
+const server = Server(app)
 const compiler = webpack(config);
+const io = socket(server)
 
 app.use(require('webpack-dev-middleware')(compiler, {  
   noInfo: true,
@@ -18,11 +22,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));  
 
+io.on('connection', (socket) => {
+  console.log('a user connected') 
+})
+
 app.get('*', function(req, res) {  
   res.sendFile(path.join( __dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {  
+server.listen(port, function(err) {  
   if (err) {
     console.log(err);
   } else {
